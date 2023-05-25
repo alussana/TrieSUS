483cbfd350c987b6af71ad268ff5ffecfed40112
alussana <alussana@ebi.ac.uk>
2023-05-25 09:46:56 +0100

    package structure completed

--- triesus/__init__.py
+++ triesus/__init__.py
@@ -5,5 +5,5 @@ Find the Smallest Unique Subset (SUS)
 """
 
 __version__ = "0.1.0"
-__author__ = 'Alessandro Lussana'
+__author__ = 'Alessandro Lussana <alessandro.lussana@proton.me>'
 __credits__ = 'European Bioinformatics Institute (EMBL-EBI)'
